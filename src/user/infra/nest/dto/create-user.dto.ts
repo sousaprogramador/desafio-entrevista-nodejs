ad72ed6cf60f4e0a6d8431eae65935d2943cb44b
Mateus Sousa <sousa.programador@gmail.com>
2023-08-30 10:03:48 -0300

    add swagger

--- src/user/infra/nest/dto/create-user.dto.ts
+++ src/user/infra/nest/dto/create-user.dto.ts
@@ -1,34 +1,29 @@
-import {
-  IsDate,
-  IsBoolean,
-  IsOptional,
-  IsString,
-  IsNotEmpty,
-} from 'class-validator';
+import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateUserUseCase } from '../../../application';
 
 export class CreateUserDto implements CreateUserUseCase.Input {
+  @ApiProperty({
+    type: 'string',
+    example: 'Jhon Snow',
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    type: 'string',
+    example: 'jhon@mail.com',
+  })
   @IsString()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    type: 'string',
+    example: 'secure',
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
-
-  @IsString()
-  @IsOptional()
-  avatar?: string | null;
-
-  @IsBoolean()
-  @IsOptional()
-  is_active?: boolean;
-
-  @IsDate()
-  @IsOptional()
-  created_at?: Date;
 }
